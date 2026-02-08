aea1fa22d9a1e5c5a3e7d61f1bfb881be7174c4c
Ismail Erdem Sirma <ismailsirma@gmail.com>
2026-02-08 15:03:38 +0300

    JPA Mapped Super Class

---
+++ src/main/java/sirmam/springdatajpa/domain/OrderHeader.java
@@ -0,0 +1,34 @@
+package sirmam.springdatajpa.domain;
+
+import jakarta.persistence.Entity;
+
+
+@Entity
+public class OrderHeader extends BaseEntity {
+
+    private String customer;
+
+    public String getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(String customer) {
+        this.customer = customer;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof OrderHeader that)) return false;
+        if (!super.equals(o)) return false;
+
+        return getCustomer() != null ? getCustomer().equals(that.getCustomer()) : that.getCustomer() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = super.hashCode();
+        result = 31 * result + (getCustomer() != null ? getCustomer().hashCode() : 0);
+        return result;
+    }
+}
