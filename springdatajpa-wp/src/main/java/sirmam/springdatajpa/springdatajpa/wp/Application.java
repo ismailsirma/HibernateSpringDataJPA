779488b36f269f3d7a4171560bf1910532812380
Ismail Erdem Sirma <ismailsirma@gmail.com>
2026-07-25 12:38:29 +0300

    Move to new folder structure

---
+++ springdatajpa-wp/src/main/java/sirmam/springdatajpa/springdatajpa/wp/Application.java
@@ -0,0 +1,13 @@
+package sirmam.springdatajpa.springdatajpa.wp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
