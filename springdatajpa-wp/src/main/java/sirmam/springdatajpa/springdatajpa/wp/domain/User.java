5b4f2c497dffb653059c8283ce5abc4c8de5cd1b
Ismail Erdem Sirma <ismailsirma@gmail.com>
2026-08-16 20:52:27 +0300

    add user and usermeta entities

---
+++ springdatajpa-wp/src/main/java/sirmam/springdatajpa/springdatajpa/wp/domain/User.java
@@ -0,0 +1,48 @@
+package sirmam.springdatajpa.springdatajpa.wp.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "wp_users")
+@Getter
+@Setter
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_login")
+    private String login;
+
+    @Column(name = "user_pass")
+    private String password;
+
+    @Column(name = "user_nicename")
+    private String nicename;
+
+    @Column(name = "user_email")
+    private String email;
+
+    @Column(name = "user_url")
+    private String url;
+
+    @Column(name = "user_registered")
+    private Timestamp registered;
+
+    @Column(name = "user_activation_key")
+    private String activationKey;
+
+    @Column(name = "user_status")
+    private Integer status;
+
+    //@Basic(optional = false)
+    @Column(name = "display_name", nullable = false)
+    private String displayName;
+}
---
+++ springdatajpa-wp/src/main/java/sirmam/springdatajpa/springdatajpa/wp/domain/UserMeta.java
@@ -0,0 +1,24 @@
+package sirmam.springdatajpa.springdatajpa.wp.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "wp_usermeta")
+@Getter
+@Setter
+@NoArgsConstructor
+public class UserMeta {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "umeta_id")
+    private Long id;
+    private Long userId;
+    private String metaKey;
+
+    @Lob
+    private String metaValue;
+}
